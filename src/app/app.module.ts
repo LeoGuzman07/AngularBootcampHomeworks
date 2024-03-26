31ce800cb04f9eecc632aacebd31259be443a5a5
Leonardo Guzman <lguzman@apexsystems.com>
2024-03-26 11:04:29 -0600

    Directivas Custom Capitalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DirectivasdeatributoModule } from './directivasdeatributo/directivasdeatributo.module';
-import { DirectivasestructuralesModule } from './directivasestructurales/directivasestructurales.module';
+import { DirectivasDeAtributoModule } from './directivasdeatributo/directivas-de-atributo.module';
+import { DirectivasEstructuralesModule } from './directivasestructurales/directivas-estructurales.module';
+import { CapitalizeDirectiveDirective } from './directivas-custom/capitalize-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizeDirectiveDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DirectivasdeatributoModule,
-    DirectivasestructuralesModule
+    DirectivasDeAtributoModule,
+    DirectivasEstructuralesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
