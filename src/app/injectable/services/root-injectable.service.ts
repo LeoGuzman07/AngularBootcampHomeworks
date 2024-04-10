7023c1f2637901367780dd3744fedd26ca1fd437
LeoGuzman07 <144833604+LeoGuzman07@users.noreply.github.com>
2024-04-10 14:58:33 -0600

    Delete src/app/injectable directory

--- src/app/injectable/services/root-injectable.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class RootInjectableService {
-  public persistentData: String = "I'm a variable inside a singleton"
-  constructor() { }
-}
